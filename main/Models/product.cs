06a0b1ad94ff2e217923304983ec1130482f2199
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-17 23:45:59 +0200

    Create product.cs

---
+++ main/Models/product.cs
@@ -0,0 +1,11 @@
+﻿namespace main.Models {
+    public class product {
+        public long ID; // primary key
+        public string[] brand;
+        public string[] model;
+        public string[] batch;
+        public string[] type;
+        public string intro;
+        public object spec;
+    }
+}
